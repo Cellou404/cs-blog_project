32e07deb38220a5a498c9b416409e7c6992927f5
cellou649@gmail.com <cellou649@gmail.com>
2022-12-26 19:57:00 +0000

    Debug=False

--- csblog/settings.py
+++ csblog/settings.py
@@ -30,9 +30,9 @@ environ.Env.read_env()
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
